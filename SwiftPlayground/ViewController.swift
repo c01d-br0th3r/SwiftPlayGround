501678bbfdc7049ca28d3699c4700d87de874776
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-05 01:39:08 +0900

    make timer, alert func

--- SwiftPlayground/ViewController.swift
+++ SwiftPlayground/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var alarmTime: String?
+    
+    @IBOutlet var lblcurrentTime: UILabel!
+    @IBOutlet var lblPickedTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickedTime.text = formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date);
+    }
+    
+    @objc func updateTime() {
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblcurrentTime.text = formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if (alarmTime == currentTime) {
+            let timeoutAlert = UIAlertController(title: "알람", message: "시간 종료 !!", preferredStyle: UIAlertController.Style.alert);
+            let okAction = UIAlertAction(title: "확인했습니다.", style: UIAlertAction.Style.default, handler: nil);
+            
+            timeoutAlert.addAction(okAction)
+            present(timeoutAlert, animated: true, completion: nil)
+        }
+    }
 }
 
