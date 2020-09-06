e5b5c53376fd10cc5285bf677c106e845bf5cb2e
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-09-06 14:44:50 +0900

    Initial State

--- SwiftPlayground/ViewController.swift
+++ SwiftPlayground/ViewController.swift
@@ -7,84 +7,12 @@
 //
 
 import UIKit
-import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-    @IBOutlet var txtUrl: UITextField!
-    @IBOutlet var myWebView: WKWebView!
-    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
-    
-    func loadWebPage(_ url: String) {
-        let myUrl = URL(string: url)
-        let myRequest = URLRequest(url: myUrl!)
-        myWebView.load(myRequest)
-    }
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        myWebView.navigationDelegate = self
-        loadWebPage("http://c01d-br0th3r.kr")
-    }
-
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        myActivityIndicator.startAnimating()
-        myActivityIndicator.isHidden = false
-    }
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        myActivityIndicator.stopAnimating()
-        myActivityIndicator.isHidden = true
-    }
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        myActivityIndicator.stopAnimating()
-        myActivityIndicator.isHidden = true
-    }
-    
-    func checkUrl(_ url: String) -> String {
-        var strUrl = url
-        let flag = strUrl.hasPrefix("http://")
-        if !flag {
-            strUrl = "http://" + strUrl
-        }
-        return strUrl
-    }
-    
-    @IBAction func btnGotoUrl(_ sender: UIButton) {
-        let myUrl = checkUrl(txtUrl.text!)
-        txtUrl.text = ""
-        loadWebPage(myUrl)
-    }
-    
-    @IBAction func btnGoSite1(_ sender: UIButton) {
-        loadWebPage("https://github.com")
-    }
-    
-    @IBAction func btnGoSite2(_ sender: UIButton) {
-        loadWebPage("https://google.com")
-    }
-    
-    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
-        let htmlString = "<h1>HTML String</h1><p>String 변수를 이용한 웹페이지</p>"
-        myWebView.loadHTMLString(htmlString, baseURL: nil)
-    }
-    
-    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
-    }
-    
-    @IBAction func btnStop(_ sender: UIBarButtonItem) {
-        myWebView.stopLoading()
-    }
-    
-    @IBAction func btnReload(_ sender: UIBarButtonItem) {
-        myWebView.reload()
-    }
-    
-    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
-        myWebView.goBack()
-    }
-    
-    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
-        myWebView.goForward()
     }
 }
 
